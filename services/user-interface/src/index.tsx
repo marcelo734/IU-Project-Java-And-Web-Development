ab4a096ff31de1d2d9e6aa8300a37d72bae0feed
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-06-26 22:04:12 -0300

    feat[ui]: install bootstrap

--- services/user-interface/src/index.tsx
+++ services/user-interface/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {RouterProvider} from "react-router-dom";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import {router} from './routes'
 
 import "./global.css"
