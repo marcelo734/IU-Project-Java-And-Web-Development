2d7ebf89789a9dc8583a4ce17ea11ae50ded54d9
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-06-26 22:04:26 -0300

    feat[ui]: adjust layout padding

--- services/user-interface/src/layout/layout.tsx
+++ services/user-interface/src/layout/layout.tsx
@@ -2,15 +2,20 @@ import React  from "react";
 import { Outlet } from 'react-router-dom';
 
 import TopBar from "./topbar/topbar";
+import styled from "styled-components";
+
+const Main = styled.main`
+    padding: 16px;
+`
 
 export default function Layout() {
     return (
         <>
             <TopBar />
 
-            <main>
+            <Main>
                 <Outlet />
-            </main>
+            </Main>
         </>
 
     )
