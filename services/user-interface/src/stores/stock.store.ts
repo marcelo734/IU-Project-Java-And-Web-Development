e06cc12b11e83c818cd865a44469abf75881d104
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-12-02 13:48:36 -0300

    feat[ui]: add env to control if use mock data or call API

--- services/user-interface/src/stores/stock.store.ts
+++ services/user-interface/src/stores/stock.store.ts
@@ -6,6 +6,7 @@ import {Stock} from "../types/Stock";
 import {stocksMockData} from "./__MOCK__/stock.mock";
 
 const apiUrl =  process.env.REACT_APP_API_URL;
+const useMockData = Boolean(process.env.REACT_APP_USE_MOCK_DATA);
 
 type State = {
     myStocks: Stock[]
@@ -21,6 +22,20 @@ export const useStore = create<State & Action>((set) => ({
     myStocks: [],
     selectedStock: null,
     async setSelectedStock(symbol: string) {
+        if (useMockData) {
+            setTimeout(() => {
+                const stock = stocksMockData
+                    .find(el => el.symbol.toLocaleLowerCase() === symbol.toLocaleLowerCase())!;
+
+                set((state) => ({
+                    ...state,
+                    selectedStock: stock
+                }))
+            }, 1500)
+
+            return
+        }
+
         const stock = await axios.get(`${apiUrl}/stocks/${symbol}`)
 
         set((state) => ({
