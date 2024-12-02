1d49ed308e2a9c69699e7a34c176848e94a06727
Marcelo Ivo <marceloimgontijo@gmail.com>
2024-12-02 19:54:51 -0300

    feat(stocks-backend): configure liquibase with postgres

---
+++ services/stocks_backend/src/main/resources/db/scripts/create-search-history-table.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+-- changeset marcelo.gontijo:enable-generate-uuid-extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- changeset marcelo.gontijo:1.0-create-search-history-table
+CREATE TABLE stocks_search_history (
+    id VARCHAR(128) not null primary key default uuid_generate_v4(),
+    symbol VARCHAR(5) not null unique,
+    date TIMESTAMP not null default CURRENT_TIMESTAMP
+)
